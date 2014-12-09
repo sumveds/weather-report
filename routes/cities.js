babfd14ae7127114a51b33840f11f5d5df821f03
Sumved Shami <sshami@paypal.com>
2014-12-09 18:18:47 +0530

    Externalized cities and states mapping.

---
+++ routes/cities.js
@@ -0,0 +1,12 @@
+module.exports = {
+	"Campbell":"CA",
+	"Omaha":"NE",
+	"Austin":"TX",
+	"Timonium":"MD",
+	"San Jose":"CA",
+	"New York":"NY",
+	"Los Angeles":"CA",
+	"San Diego":"CA",
+	"San Francisco":"CA",
+	"San Antonio":"TX"
+};
--- routes/weather.js
+++ routes/weather.js
@@ -4,6 +4,8 @@ var async = require('async');
 var express = require('express');
 var router = express.Router();
 
+var _db = require('./cities');
+
 router.get('/', function (req, res) {
 	res.render('weather', { title: 'Weather reporting tool' });
 });
@@ -11,24 +13,7 @@ router.get('/', function (req, res) {
 /* GET weather reporting page. */
 router.post('/report', function(req, res) {
 
-	console.log(req.body);
-
 	var cities = req.body.cities.split(',');
-
-	// console.log(cities);
-
-	/*var _db = [
-		{city: "Campbell", state: "CA"},
-		{city: "Omaha", state: "NE"},
-		{city: "Austin", state: "TX"},
-		{city: "Timonium", state: "MD"}
-	];*/
-	var _db = {
-		"Campbell":"CA",
-		"Omaha":"NE",
-		"Austin":"TX",
-		"Timonium":"MD"
-	};
 
 	var reports = [];
 
@@ -44,7 +29,8 @@ router.post('/report', function(req, res) {
         	response.on('end', function() {
         		var fullWeatherReport = JSON.parse(body);
         		if(fullWeatherReport.current_observation == null) {
-        			callback(getError({city: city, state: _db[city]}, 404, 'Resource not found'));
+        			// callback(getError({city: city, state: _db[city]}, 404, 'Resource not found'));
+        			console.error(getError({city: city, state: _db[city]}, 404, 'Resource not found'));
         		} else {
 	        		reports.push(getMinimalWeatherReport(fullWeatherReport));
 	        		callback();
@@ -68,38 +54,6 @@ router.post('/report', function(req, res) {
 			res.render('report', { reports: reports, title: 'Weather reporting tool' });
 		}
 	});
-
-	/*var reports = [
-        {
-                "city": "Omaha, NE",
-                "location": "-95.93376160,41.26331329",
-                "weather": "Clear",
-                "temperature": "47.6 F (8.7 C)",
-                "humidity": "56%"
-        },
-        {
-                "city": "Timonium, MD",
-                "location": "-76.65486145,39.43494034",
-                "weather": "Overcast",
-                "temperature": "32.9 F (0.5 C)",
-                "humidity": "63%"
-        },
-        {
-                "city": "Campbell, CA",
-                "location": "-121.95592499,37.27999878",
-                "weather": "Partly Cloudy",
-                "temperature": "66.9 F (19.4 C)",
-                "humidity": "71%"
-        },
-        {
-                "city": "Austin, TX",
-                "location": "-97.74169922,30.27115822",
-                "weather": "Clear",
-                "temperature": "62.8 F (17.1 C)",
-                "humidity": "66%"
-        }
-	];
-	res.render('report', { reports: reports, title: 'Weather reporting tool' });*/
 });
 
 router.all('/', function(req, res) {
